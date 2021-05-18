ebd6a83e885c801d252c559a4471dc93e67f9fee
Javi Viñas <javi.vinas.2000@gmail.com>
2021-05-18 02:43:09 +0200

    Modificaciones varias de estilo en los comentarios

--- src/components/movie/CommentList.js
+++ src/components/movie/CommentList.js
@@ -69,16 +69,16 @@ function Comment({comment}) {
     return <li className={`w-full rounded-md bg-white overflow-hidden relative shadow-lg`}
                style={{aspectRatio: '6/3'}}
     >
-        <article className='p-2'>
+        <article className='p-4'>
             <div className='flex justify-between content-center'>
-                <div className=' flex-1 font-bold text-gray-800'>
+                <div className=' flex-1 font-bold text-gray-800 m-2'>
                     {comment?.user?.email}
                 </div>
                 <div className='flex-1'>
                     <StaticStarRating rating={comment.rating}/>
                 </div>
             </div>
-            <div>
+            <div className='m-6'>
                 {comment.comment}
             </div>
         </article>
